719e099113a170bb75c6942ce7d448fe71ff15eb
wlswodnd <83488101+wlswodnd@users.noreply.github.com>
2021-06-06 14:46:19 +0900

    파일 생성

---
+++ index.py
@@ -0,0 +1,6 @@
+import pygame   # 파이게임 불러오기
+import sys  # 시스템 불러오기
+from pygame import *    # 파이게임에 모든 요소 불러오기
+import random   # 난수 불러오기
+background = pygame.display.set_mode(0,0,0) # 검은 화면
+pygame.init()
